0fece7290f6be0eae59221027f32dcc96fb3ce3a
Rafael Mendes <joserafaelmb@gmail.com>
2016-03-05 18:33:02 +0000

    modifying to test something on git

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -13,7 +13,9 @@ $this->pageTitle=Yii::app()->name;
 	<li>View file: <code><?php echo __FILE__; ?></code></li>
 	<li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
 </ul>
+changing...
 
+<br />
 <p>For more details on how to further develop this application, please read
 the <a href="http://www.yiiframework.com/doc/">documentation</a>.
 Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
